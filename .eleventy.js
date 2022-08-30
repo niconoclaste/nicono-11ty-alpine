f5b9c7d8788dadce4b301af0845c31010bbc04e0
NicoNoClaste <niconoclaste@users.noreply.github.com>
2022-08-30 21:50:51 +0900

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function(eleventyConfig) {
+  // eleventyConfig.addWatchTarget('src/assets/styles.css');
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/alpinejs/dist/cdn.js': './assets/js/alpine.js',
+  });
+  eleventyConfig.addPassthroughCopy('src/assets/styles.css');
+  eleventyConfig.setBrowserSyncConfig({
+    open: true,
+  });
+  return {
+    // When a passthrough file is modified, rebuild the pages:
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
